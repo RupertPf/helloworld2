dbe39e70e74010d903dcd6775f05864fce7cf2e1
Rupert Pfiser <rupert.pfister@gmail.com>
2023-07-18 18:06:08 +0200

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        // <Datentyp> <VariableName> = <Wert>
+        final boolean b = true;//Logische Werte
+        b = false;
+        char c = 'r'; //Buchstaben
+        short s = 32767;
+        int i = 420000000;
+        long l = 4200000000000000000L;
+        float f = 4.9F;
+        double d = 4.4532534253523425;
+        byte b1 = 127;
+    }
+}
